ea40a4b6272dd80f8ff0f35275cbace89baeb575
Thomas TORRES <114108420+Thomas22206512@users.noreply.github.com>
2025-06-18 01:19:04 +0200

    FirstCommit

---
+++ src/main/java/net/renard/MysfallDataGenerator.java
@@ -0,0 +1,11 @@
+package net.renard;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MysfallDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
